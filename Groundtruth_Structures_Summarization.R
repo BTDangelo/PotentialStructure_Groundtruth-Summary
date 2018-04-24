2ab57821888e7a893a515012000ea70333c1fa53
BTDangelo <bernadette.dangelo@state.or.us>
2018-04-24 15:50:59 -0700

    code for summarization of structure data

--- Groundtruth_Structures_Summarization.R
+++ Groundtruth_Structures_Summarization.R
@@ -3,34 +3,39 @@
 # Path to file containing potential structure data
 strct.gt.data <- "E:/Documents/Ground_Truthing_Summarization/Upper Yaquina River Structure Groundtruth (Responses) - 20180315.csv"
 
+
 # Read the .csv file 
-gt.data <- read.csv(strct.gt.data, header = TRUE)
+str.gt.data <- read.csv(strct.gt.data, header = TRUE)
 
 
 # Add column named "Group" to response data frame 
-gt.data["Group"] 
+#str.gt.data["Group"] 
+
+
+# Column Names
+colnames(str.gt.data)
+
+
 
 # Designate the group of each response using the conditions in Table 1 (in word document)
-gt.data$Group <- ifelse(gt.data$Is.at.least.one.structure.visible.on.imagery.within.100.feet.current.potential.structure..no...go.to.bottom.of.page.and.submit.form. == "Yes", "E", "NA")  
-gt.data$Group <- ifelse(gt.data[,4] == "No" & gt.data[,5] == "No" & gt.data[,6] == "No" & gt.data[,8] == "No", "A", 
-                 ifelse(gt.data[,4] == "No" & gt.data[,5] == "No" & gt.data[,6] == "No" & gt.data[,8] == "Yes", "B", 
-                 ifelse(gt.data[,4] == "Yes", "C", 
-                 ifelse(gt.data[,5] == "Yes", "D", 
-                 ifelse(gt.data[,6] == "Yes", "E", "NA")))))    
+
+str.gt.data$Group <- ifelse(str.gt.data[,4] == "No" & str.gt.data[,5] == "No" & str.gt.data[,6] == "No" & str.gt.data[,8] == "No", "A", 
+                     ifelse(str.gt.data[,4] == "No" & str.gt.data[,5] == "No" & str.gt.data[,6] == "No" & str.gt.data[,8] == "Yes", "B", 
+                     ifelse(str.gt.data[,4] == "Yes", "C", 
+                     ifelse(str.gt.data[,5] == "Yes", "D", 
+                     ifelse(str.gt.data[,6] == "Yes", "E", "NA")))))    
+
 
 
 # Create summary table for each group that has counts of structures in each group by Column B (Reviewer)
 
+tapply(str.gt.data[,2], str.gt.data$Group, summary)
+
+
+# Table1 <- aggregate.data.frame(gt.data[,2], list(gt.data$Group), FUN=summary)
 
 # Save the response and summary data frames to a file (or files)
-
-
-
-
-
-
-# Column Names
-colnames(gt.data)
+write.csv(str.gt.data, "E:/Documents/Ground_Truthing_Summarization/Upper_Yaquina_River_Structure_Groundtruth_Response_BTD.csv")
 
 
 
