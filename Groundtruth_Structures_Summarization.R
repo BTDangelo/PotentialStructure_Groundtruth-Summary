f94f26a7852194c7e91c1be07992ba768d122d27
BTDangelo <bernadette.dangelo@state.or.us>
2018-04-24 14:34:06 -0700

    Designated group for each response using conditions from Table 1

--- Groundtruth_Structures_Summarization.R
+++ Groundtruth_Structures_Summarization.R
@@ -6,5 +6,32 @@ strct.gt.data <- "E:/Documents/Ground_Truthing_Summarization/Upper Yaquina River
 # Read the .csv file 
 gt.data <- read.csv(strct.gt.data, header = TRUE)
 
+
 # Add column named "Group" to response data frame 
+gt.data["Group"] 
+
 # Designate the group of each response using the conditions in Table 1 (in word document)
+gt.data$Group <- ifelse(gt.data$Is.at.least.one.structure.visible.on.imagery.within.100.feet.current.potential.structure..no...go.to.bottom.of.page.and.submit.form. == "Yes", "E", "NA")  
+gt.data$Group <- ifelse(gt.data[,4] == "No" & gt.data[,5] == "No" & gt.data[,6] == "No" & gt.data[,8] == "No", "A", 
+                 ifelse(gt.data[,4] == "No" & gt.data[,5] == "No" & gt.data[,6] == "No" & gt.data[,8] == "Yes", "B", 
+                 ifelse(gt.data[,4] == "Yes", "C", 
+                 ifelse(gt.data[,5] == "Yes", "D", 
+                 ifelse(gt.data[,6] == "Yes", "E", "NA")))))    
+
+
+# Create summary table for each group that has counts of structures in each group by Column B (Reviewer)
+
+
+# Save the response and summary data frames to a file (or files)
+
+
+
+
+
+
+# Column Names
+colnames(gt.data)
+
+
+
+
